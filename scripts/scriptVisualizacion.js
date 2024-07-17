ddd3953464036114323d63924357e243ab16ae1c
Alejandro Diaz <129427891+DNosheZ@users.noreply.github.com>
2024-07-17 13:09:09 -0500

    v.1.3.2
    1st sprint

--- scripts/scriptVisualizacion.js
+++ scripts/scriptVisualizacion.js
@@ -1,65 +1,116 @@
+let registroPara5N;
+let registroPara7N;
+let registroPara6S;
+
 document.getElementById('dateForm').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    if(localStorage.length!==0 && document.getElementById('dateInput').value){
+    if (localStorage.length !== 0 && document.getElementById('dateInput').value) {
         const dateInput = document.getElementById('dateInput').value;
         const [year, month, day] = dateInput.split('-');
-
         const formattedDate = `${day}/${month}/${year}`;
         console.log(formattedDate);
-        let found=false;
+
+        let found = false;
         for (let i = 0; i < localStorage.length; i++) {
             const key = localStorage.key(i);
-            //en caso que se halle una llave con esta fecha, se revisa si es del 5N, 7N o 6S, y segun eso, se guarda el JSON correspondiente para mostrarse
-            const keyDatePart = key.split('-')[0]; 
-            const keyRoomPart = key.split('-')[1]; 
-            console.log(keyDatePart);
+            const keyDatePart = key.split('-')[0];
+            const keyRoomPart = key.split('-')[1];
+
             if (keyDatePart === formattedDate) {
-                found=true;
-                if(keyRoomPart==="5N"){
-                    const registroPara5N=JSON.parse(localStorage.getItem(key));
-                }else if(keyRoomPart==="7N"){
-                    const registroPara7N=JSON.parse(localStorage.getItem(key));
-                }else if(keyRoomPart==="6S"){
-                    const registroPara6S =JSON.parse(localStorage.getItem(key));   
+                found = true;
+                if (keyRoomPart === "registro5N") {
+                    registroPara5N = JSON.parse(localStorage.getItem(key));
+                    console.log(registroPara5N);
+                } else if (keyRoomPart === "registro7N") {
+                    registroPara7N = JSON.parse(localStorage.getItem(key));
+                    console.log(registroPara7N);
+                } else if (keyRoomPart === "registro6S") {
+                    registroPara6S = JSON.parse(localStorage.getItem(key));
+                    console.log(registroPara6S);
                 }
             }
         }
-        if(found){
+        if (found) {
+            document.getElementById("register-option").style.display='none';
             redirectTo('review-date', 'selection-of-service');
-            Registros6S.forEach((registro, index) => {
-                const formGroup = document.createElement('div');
-                formGroup.className = 'form-group';
-        
-                const h4 = document.createElement('h4');
-                h4.class = `servicio-${index}-procedencia-visual`;
-                h4.textContent = registro.nombre;
-        
-                const p = document.createElement('p');
-                p.class = `servicio-${index}-cantidad`;
-                p.textContent = registro.cantidad;
-        
-                formGroup.appendChild(h4);
-                formGroup.appendChild(p);
-                form.appendChild(formGroup);
-            });
-        }else{
-            alert("No hay registros de esta fecha, por favor prueba con otra")
+        } else {
+            alert("No hay registros de esta fecha, por favor prueba con otra");
         }
-
-    }else if(!document.getElementById('dateInput').value){
+    } else if (!document.getElementById('dateInput').value) {
         alert("Por favor escoge una fecha");
-    }else{
-        alert("No hay registros de alguna sala")
+    } else {
+        alert("No hay registros de alguna sala");
     }
-
-    
 });
 
-document.getElementById('selecService').addEventListener('submit', function(event) {
+document.getElementById('select-service-to-visualizate').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    var act = event.submitter.value;
-    if (act === '5N') {}
-});
+    const bedsDiv = document.getElementById("bedsVisualization");
+    const act = event.submitter.value;
 
+    bedsDiv.innerHTML = ''; // Limpiar el contenido existente
+    if (act === '5N' && registroPara5N) {
+        redirectTo('selection-of-service', 'service-visualization');
+        document.getElementById('visualizacion-sala-i').innerHTML = "5N";
+        console.log(registroPara5N);
+        registroPara5N.forEach(servicio => {
+            const serviceElement = document.createElement('div');
+            serviceElement.className="form-container";
+            const h4=document.createElement('h4');
+            const p=document.createElement('p');
+            h4.textContent=`${servicio.nombre} : `;
+            p.textContent=`${servicio.cantidad}`;
+            serviceElement.appendChild(h4);
+            serviceElement.appendChild(p);
+            bedsDiv.appendChild(serviceElement);
+        });
+    } else if (act === '7N' && registroPara7N) {
+        redirectTo('selection-of-service', 'service-visualization');
+        document.getElementById('visualizacion-sala-i').innerHTML = "7N";
+        console.log(registroPara7N);
+        registroPara7N.forEach(servicio => {
+            const serviceElement = document.createElement('div');
+            serviceElement.className="form-container";
+            const h4=document.createElement('h4');
+            const p=document.createElement('p');
+            h4.textContent=`${servicio.nombre} : `;
+            p.textContent=`${servicio.cantidad}`;
+            serviceElement.appendChild(h4);
+            serviceElement.appendChild(p);
+            bedsDiv.appendChild(serviceElement);
+        }); 
+        bedsDiv.appendChild();
+    } else if (act === '6S' && registroPara6S) {
+        redirectTo('selection-of-service', 'service-visualization');
+        document.getElementById('visualizacion-sala-i').innerHTML = "6S";
+        console.log(registroPara6S);
+        registroPara6S.forEach(servicio => {
+            const serviceElement = document.createElement('div');
+            serviceElement.className="form-container";
+            const h4=document.createElement('h4');
+            const p=document.createElement('p');
+            h4.textContent=`${servicio.nombre} : `;
+            p.textContent=`${servicio.cantidad}`;
+            serviceElement.appendChild(h4);
+            serviceElement.appendChild(p);
+            bedsDiv.appendChild(serviceElement);
+        });
+    } else {
+        alert("No hay datos disponibles para esta sala.");
+    }
+});
+document.getElementById('back-visualization-room-to-general-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    redirectTo('service-visualization','selection-of-service');
+});
+document.getElementById('back-from-visualization-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    redirectTo('selection-of-service','review-date');
+    document.getElementById("register-option").style.display='block';
+});
+document.getElementById('back-from-date-select-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    window.location.href = 'menu.html';
+});
